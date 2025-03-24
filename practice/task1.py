27b05609a199303779e0ae6e1d9981d577cd9862
AigerimAkumo <llc.ziolik@gmail.com>
2025-03-24 20:37:34 +0000

    adding practice

---
+++ practice/task1.py
@@ -0,0 +1,13 @@
+# def sleep_in(weekday, vacation):
+#   if not weekday or vacation:
+#     return True
+#   else:
+#       return False
+  
+  
+  
+#   def monkey_trouble(a_smile, b_smile):
+#   if a_smile==b_smile:
+#     return True
+#   else:
+#     return False
