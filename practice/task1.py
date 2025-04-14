9ab3552f8b2768a8ac1d0ec0a28b7d3198ff1c39
AigerimAkumo <llc.ziolik@gmail.com>
2025-04-14 14:28:49 -0500

    Update task1.py

--- practice/task1.py
+++ practice/task1.py
@@ -1,3 +1,10 @@
+
+# Arrange the code boxes in the correct positions in order to obtain a loop which executes its body with the level variable 
+# going through values 5, 3 ,
+# and 1 (in the same order).
+
+
+
 # def sleep_in(weekday, vacation):
 #   if not weekday or vacation:
 #     return True
@@ -23,3 +30,17 @@
 #     else:
 #         return False
 
+# A set of elementary operations that can be performed by a CPU is called:
+# A. an instruction list
+# B. a binary code
+# C. the ASCII code
+# D. an assembly order
+# Correct Answer: A
+
+
+# A process in which the source code is immediately executed without the need to translate it into machine code is called:
+# A. linking
+# B. interpretation
+# C. compilation
+# D. translation
+# Correct Answer: B
