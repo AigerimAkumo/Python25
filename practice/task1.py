35b8d6cb2fed1fea5443f32252e3826fbb5d9494
AigerimAkumo <llc.ziolik@gmail.com>
2025-04-14 14:30:17 -0500

    Update task1.py

--- practice/task1.py
+++ practice/task1.py
@@ -44,3 +44,166 @@
 # C. compilation
 # D. translation
 # Correct Answer: B
+
+##########################################################
+
+# The pyc file contains ...
+# A. a Python interpreter.
+# B. Python source code.
+# C. compiled Python bytecode.  # C 
+# D. a Python compiler.
+
+
+# Select the true statements: (Choose two.)
+# A. You can use keywords as variable names in Python
+# B. You can use keywords as function names in Python
+# C. You cannot use keywords as variable names in Python   # C
+# D. You cannot use keywords as function names in Python   # D
+
+# What do you call a computer program which directly executes instructions written in a programming language?
+# A. A compiler
+# B. An interpreter
+# C. A translator
+# Correct Answer: B
+
+# The escape character owes its name to the fact that it:
+# A. cannot be caught due to its high speed
+# B. escapes from source files into the computer memory
+# C. changes the meaning of the character next to it     # C
+
+# What do you call a command-line interpreter which lets you interact with your OS and execute Python commands and scripts?
+# A. A console
+# B. A compiler
+# C. Jython
+# D. An editor
+
+
+# A value returned by the input() function is:
+# A. an integer
+# B. a float
+# C. a string
+# Correct Answer: C
+
+
+# What is true about compilation? (Choose two.)
+# A. The code is converted directly into machine code executable by the processor
+# B. It tends to be faster than interpretation
+# C. It tends to be slower than interpretation
+# D. Both you and the end user must have the code
+# Correct Answer: A B
+
+# The compiled Python bytecode is stored in files having names ending with:
+# A. py
+# B. pyb
+# C. pyc
+# D. pc
+# Correct Answer: C
+
+
+
+# The print() function is an example of:
+# A. a Python built-in function
+# B. a user-defined function
+# C. an anonymous function
+# D. a Python output method
+# Correct Answer: A
+
+
+# A keyword is a word: (Choose two.)
+# A. that cannot be used as a variable name
+# B. that cannot be used as a function name
+# C. is the most important word in the whole program
+# Correct Answer: A B
+
+
+# UTF 8 is ...
+# A. the 9th version of the UTF Standard.
+# B. a synonym for "byte".
+# C. a Python version name.
+# D. an encoding form of the Unicode Standard.
+# Correct Answer: D
+
+# A complete set of known commands is called:
+# A. a machine list
+# B. a low-level list
+# C. an instruction list
+# Correct Answer: C
+
+
+# What do you call a file containing a program written in a high-level programming language?
+# A. A code file
+# B. A target file
+# C. A machine file
+# D. A source file
+# Correct Answer: D
+
+# What is source code?
+# A. A program written in a high-level programming language
+# B. Another name for a source file
+# C. Machine code executed by computers
+# Correct Answer: A
+
+
+# The folder created by Python used to store pyc files is named:
+# pyc__
+# pycfiles__
+# pycache__
+# D. __cache__
+# Correct Answer: C
+
+
+# If a list passed into a function as an argument, deleting any of its elements inside the function using the del instruction:
+# A. will not affect the argument
+# B. will cause a runtime error
+# C. will affect the argument
+# Correct Answer: C
+
+
+# A function definition:
+# A. cannot be placed among other code
+# B. may be placed anywhere inside the code after the first invocation
+# C. must be placed before the first invocation
+# Correct Answer: C
+
+
+# Which of the following items are present in the function header?
+# A. function name
+# B. return value
+# C. parameter list
+# D. function name and parameter list
+# Correct Answer: D
+
+
+# The meaning of the keyword parameter is determined by:
+# A. its value
+# B. its connection with existing variables
+# C. its position within the argument list
+# D. the argument's name specified along with its value
+# Correct Answer: D
+
+# The part of your code where you think an exception may occur should be placed inside:
+# A. the except: branch
+# B. the exception: branch
+# C. the try: branch
+# Correct Answer: C
+
+# The part of your code where the handling of an exception takes place should be placed inside:
+# A. the except: branch
+# B. the exception: branch
+# C. the try: branch
+# Correct Answer: A
+
+# How many arguments can the print() function take?
+# A. Any number of arguments (excluding zero).
+# B. Not more than seven arguments.
+# C. Any number of arguments (including zero).
+# D. Just one argument.
+# Correct Answer: C
+
+
+# Which of the following statements is false?
+# A. The None value can be compared with variables.
+# B. The None value can not be used as an argument of arithmetic operators.
+# C. The None value may not be used outside functions.
+# D. The None value can be assigned to variables.
+# Correct Answer: C
